2a26cc2b9537e7f4394d1027c6713aa08b9746fa
torcuata22 <marilynjmarquez@gmail.com>
2023-08-14 15:33:59 -0400

    auth works

--- crud.rb
+++ crud.rb
@@ -26,9 +26,19 @@ def create_sercure_users(list_of_users)
     list_of_users
 end
 
-puts create_sercure_users(users)
+new_users =  create_sercure_users(users)
+puts new_users
 
-
+def authenticate_user(username, password, list_of_users)
+    list_of_users.each do |user_record|
+    
+        if user_record[:username] == username && verify_hash_digest(user_record[:password]) == password
+            return user_record
+        end
+    end 
+    "Credentials were incorrect"
+end
 # new_password = create_hash_digest("password1")
 # puts new_password == "password1"
 
+p authenticate_user("heisenberg", "password5", new_users)
